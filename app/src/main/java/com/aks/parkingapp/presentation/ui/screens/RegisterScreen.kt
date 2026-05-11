d687ee7574fea2dba7e9a3411bd8509636dffb4c
Aksh-dev-21 <akshaychikhalekar21@gmail.com>
2026-05-12 02:57:49 +0530

    Onbording and registartion pages changes

--- app/src/main/java/com/aks/parkingapp/presentation/ui/screens/RegisterScreen.kt
+++ app/src/main/java/com/aks/parkingapp/presentation/ui/screens/RegisterScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -28,12 +27,24 @@ import com.aks.parkingapp.R
 import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextField
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import coil.compose.rememberAsyncImagePainter
@@ -44,25 +55,15 @@ import java.io.File
 @Composable
 fun RegisterScreen(navController: NavController){
 
-    Box(modifier = Modifier.fillMaxSize()){
-
-        Image(
-            painter = painterResource(R.drawable.background),
-            contentDescription = null,
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Crop
-        )
-
-
         Scaffold(
             containerColor = Color.Transparent,
             topBar = {
                 TopAppBar(
                     modifier = Modifier.statusBarsPadding(),
-                    title = { Text("New user Creation") },
+                    title = { Text("Sign up", style = TextStyle(fontSize = 16.sp, fontStyle = FontStyle.Normal)) },
                     colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = Color.Transparent,          // background color
-                        titleContentColor = Color.Black        // title text color
+                        containerColor = MaterialTheme.colorScheme.primary,          // background color
+                        titleContentColor = Color.White        // title text color
                     )
                 )
             }
@@ -76,141 +77,30 @@ fun RegisterScreen(navController: NavController){
             ) {
                 //  Greeting()
 
-                Column(modifier = Modifier.fillMaxSize()) {
+                Column(modifier = Modifier.fillMaxSize()
+                    .verticalScroll(rememberScrollState())
+                    .imePadding()
+                    .padding(16.dp),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.Start,
+                ) {
 
 
-                   // ImagePickAndShow()
-
-                    CameraWithPermissionExample()
-
+                    Spacer(modifier = Modifier.size(20.dp))
+                    Text(text = "Verify your mobile number", style = TextStyle(fontStyle = FontStyle.Normal, fontSize = 22.sp,fontWeight = FontWeight.Bold))
+                    Spacer(modifier = Modifier.size(5.dp))
+                    Text(text = "Verify your mobile number", style = TextStyle(fontStyle = FontStyle.Normal, fontSize = 14.sp))
+                    Spacer(modifier = Modifier.size(30.dp))
+                    Text(text = "Enter mobile number.*", style = TextStyle(fontStyle = FontStyle.Normal, fontSize = 14.sp))
+                    Spacer(modifier = Modifier.size(5.dp))
+                    TextField(
+                        modifier = Modifier.fillMaxSize(),
+                        value = "", onValueChange = {},
+                        )
 
                 }
 
             }
         }
 
-    }
 }
-
-
-//@Composable
-//fun ImagePickAndShow(){
-//
-//    var imageUri by remember { mutableStateOf<Uri?>(null) }
-//
-//    val launcher = rememberLauncherForActivityResult(
-//        contract = ActivityResultContracts.GetContent()
-//    ) { uri: Uri? ->
-//        imageUri = uri
-//    }
-//
-//
-//    Column(
-//        modifier = Modifier
-//            .fillMaxSize()
-//            .padding(16.dp),
-//        horizontalAlignment = Alignment.CenterHorizontally
-//    ) {
-//
-//        // Show image if selected
-//        imageUri?.let {
-//            Image(
-//                painter = rememberAsyncImagePainter(it),
-//                contentDescription = "Selected Image",
-//                modifier = Modifier
-//                    .size(200.dp)
-//                    .padding(bottom = 16.dp),
-//                contentScale = ContentScale.Crop
-//            )
-//        }
-//
-//        Button(onClick = {
-//            launcher.launch("image/*")
-//        }) {
-//            Text("Pick Image")
-//        }
-//    }
-//
-//}
-
-@Composable
-fun CameraWithPermissionExample() {
-
-    val context = LocalContext.current
-    var imageUri by remember { mutableStateOf<Uri?>(null) }
-
-    // Create image URI
-    fun createImageUri(): Uri {
-        val imageFile = File(
-            context.cacheDir,
-            "camera_${System.currentTimeMillis()}.jpg"
-        )
-        return FileProvider.getUriForFile(
-            context,
-            "${context.packageName}.provider",
-            imageFile
-        )
-    }
-
-    // Camera launcher
-    val cameraLauncher = rememberLauncherForActivityResult(
-        ActivityResultContracts.TakePicture()
-    ) { success ->
-        if (!success) {
-            imageUri = null
-        }
-    }
-
-    // Permission launcher
-    val permissionLauncher = rememberLauncherForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted ->
-        if (isGranted) {
-            imageUri = createImageUri()
-            cameraLauncher.launch(imageUri!!)
-        }
-    }
-
-    fun checkAndOpenCamera() {
-        when {
-            ContextCompat.checkSelfPermission(
-                context,
-                Manifest.permission.CAMERA
-            ) == PackageManager.PERMISSION_GRANTED -> {
-                imageUri = createImageUri()
-                cameraLauncher.launch(imageUri!!)
-            }
-
-            else -> {
-                // Request permission again on every click
-                permissionLauncher.launch(Manifest.permission.CAMERA)
-            }
-        }
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
-        imageUri?.let {
-            Image(
-                painter = rememberAsyncImagePainter(it),
-                contentDescription = "Captured Image",
-                modifier = Modifier.size(220.dp),
-                contentScale = ContentScale.Crop
-            )
-        }
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        Button(onClick = {
-            checkAndOpenCamera()
-        }) {
-            Text("Capture Image")
-        }
-    }
-}
-
