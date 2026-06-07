e8724d2081f84014a15769b63d69c3b8475753fd
Aksh-dev-21 <akshaychikhalekar21@gmail.com>
2026-06-07 21:12:04 +0530

    Add interceptor for api logs checking

--- app/src/main/java/com/aks/parkingapp/di/AppModule.kt
+++ app/src/main/java/com/aks/parkingapp/di/AppModule.kt
@@ -2,6 +2,7 @@ package com.aks.parkingapp.di
 
 import android.content.Context
 import androidx.room.Room
+import com.aks.parkingapp.BuildConfig
 import com.aks.parkingapp.data.local.dao.UserDao
 import com.aks.parkingapp.data.local.db.AppDatabase
 import com.aks.parkingapp.data.local.dao.VehicleDao
@@ -12,9 +13,12 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
+
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -42,12 +46,41 @@ object AppModule {
     }
 
 
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor():
+            HttpLoggingInterceptor {
+
+        return HttpLoggingInterceptor().apply {
+
+            level =
+                if (BuildConfig.DEBUG)
+                    HttpLoggingInterceptor.Level.BODY
+                else
+                    HttpLoggingInterceptor.Level.NONE
+        }
+    }
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit =
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+
+        return OkHttpClient.Builder()
+            .addInterceptor(
+                loggingInterceptor
+            )
+            .build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofit( okHttpClient: OkHttpClient): Retrofit =
         Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
